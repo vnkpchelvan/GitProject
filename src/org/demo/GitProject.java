9dfa09967423ba647ad5c4786d8802aa971e47fa
vnkpchelvan <porchelvan2010@gmail.com>
2024-07-31 21:30:19 +0530

    add to kart code added

--- src/org/demo/GitProject.java
+++ src/org/demo/GitProject.java
@@ -4,6 +4,12 @@ public class GitProject {
 	
 	public static void main(String[] args) {
 		System.out.println("New code");
+		
 	}
+	private void addToKart() {
+		System.out.println("print add to kart");
+
+	}
+	
 
 }
